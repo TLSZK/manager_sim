fff39323c4059e6f53adee1b2efc8688dd391d8e
Bence Bakos <bence070216@gmail.com>
2026-03-12 20:51:19 +0100

    server fix

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -1,7 +1,8 @@
+// @ts-ignore
 if (!window.crypto.randomUUID) {
   // @ts-ignore
   window.crypto.randomUUID = function() {
-    return (([1e7] as any)+-1e3+-4e3+-8e11+-1e11).replace(/[018]/g, (c: any) =>
+    return ("" + 1e7 + -1e3 + -4e3 + -8e11 + -1e11).replace(/[018]/g, (c: any) =>
       (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
     );
   };
